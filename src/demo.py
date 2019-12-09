f76ab20cbffb0b06ad25a846e3e654a79b1103bf
YanwenLin <yanwenl@andrew.cmu.edu>
2019-12-09 12:13:29 +0000

    update demo stage

--- src/demo.py
+++ src/demo.py
@@ -37,7 +37,10 @@ def init():
     # 包括:vocab,训练集/验证集各自的输入/输出索引序列
     data = torch.load(params.temp_pt_file)
     vocab = data['vocab']
+    vocab_pos = data['vocab_pos']
+    vocab_ner = data['vocab_ner']
     params = data['params']
+    params.lexical_feature = True
 
     if params.rnnsearch:
         from rnnsearch import Model
@@ -49,7 +52,7 @@ def init():
         logger.info('参数列表:{}'.format(params))
 
     # 定义模型
-    model = Model(params, vocab).to(params.device)
+    model = Model(params, vocab, vocab_pos=vocab_pos, vocab_ner=vocab_ner).to(params.device)
 
     # 如果参数中设置了打印模型结构,则打印模型结构
     if params.print_model:
@@ -68,10 +71,11 @@ def init():
     # 定义生成器
     generator = Generator(params, model)
 
-    return logger, params, vocab, model, generator
+    return logger, params, vocab, vocab_pos, vocab_ner, model, generator
 
 
-def demo(input_sentence, input_answer, logger, params, vocab, model, generator):
+def demo(input_sentence, input_answer, input_ner, input_answer_ner, input_pos, input_answer_pos, logger, params, vocab, vocab_ner, vocab_pos, model, generator):
+
     '''
     作用:
     模型和参数初始化
@@ -94,9 +98,17 @@ def demo(input_sentence, input_answer, logger, params, vocab, model, generator):
     input_sentence = '<cls> ' + input_sentence + ' <sep> ' + input_answer + ' <sep>'
     logger.info('输入句子的文本形式为 : {}'.format(input_sentence))
 
+    input_ner = '<cls> ' + input_ner + ' <sep> ' + input_answer_ner + ' <sep>'
+    input_pos = '<cls> ' + input_pos + ' <sep> ' + input_answer_pos + ' <sep>'
+
     # 将文本转化为list,并添加起止符<s>和</s>
     input_sentence = input_sentence.split()
     input_sentence = ['<s>'] + input_sentence + ['</s>']
+
+    input_ner = input_ner.split()
+    input_ner = ['<s>'] + input_ner + ['</s>']
+    input_pos = input_pos.split()
+    input_pos = ['<s>'] + input_pos + ['</s>']
 
     # 将输入句子转化为索引形式
     input_indices = vocab.convert_sentence2index(input_sentence)
@@ -108,8 +120,21 @@ def demo(input_sentence, input_answer, logger, params, vocab, model, generator):
     input_indices = input_indices.unsqueeze(0)
     # input_indices: [1(batch_size), input_seq_len]
 
+    # convert NER indices
+    ner_input_indices = vocab_ner.convert_sentence2index(input_ner)
+    ner_input_indices = torch.tensor(ner_input_indices).to(params.device)
+    ner_input_indices = ner_input_indices.unsqueeze(0)
+
+    # convert POS indices
+    pos_input_indices = vocab_pos.convert_sentence2index(input_pos)
+    pos_input_indices = torch.tensor(pos_input_indices).to(params.device)
+    pos_input_indices = pos_input_indices.unsqueeze(0)
+
     # 使用beam_search算法,以<s>作为开始符得到完整的预测序列
-    indices_pred, scores_pred = generator.generate_batch(input_indices, src_ans=None)
+    indices_pred, scores_pred = generator.generate_batch(input_indices, src_ans=None,
+                                                        pos_input_indices=pos_input_indices,
+                                                        ner_input_indices=ner_input_indices)
+
     # indices_pred: [batch_size, beam_size, output_seq_len]
     output_indices = indices_pred[0][0]
     # output_indices: [output_seq_len]
@@ -126,13 +151,19 @@ def demo(input_sentence, input_answer, logger, params, vocab, model, generator):
 if __name__ == '__main__':
 
     # 模型和参数初始化
-    logger, params, vocab, model, generator = init()
+    logger, params, vocab, vocab_ner, vocab_pos, model, generator = init()
 
     # 测试demo: 输入句子和答案,输出问题
     output_question = demo(input_sentence = 'There are 5000000 people in the united states .',
                     input_answer = '5000000',
+                    input_ner = 'O O O O O O O O O',
+                    input_answer_ner = 'O',
+                    input_pos = 'O O O O O O O O O',
+                    input_answer_pos = 'O',
                     logger = logger,
                     params = params,
                     vocab = vocab,
+                    vocab_ner = vocab_ner,
+                    vocab_pos = vocab_pos,
                     model = model,
                     generator = generator)
